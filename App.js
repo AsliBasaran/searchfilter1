38acb63faff60dfbf75dffe3fa7b41362352a1dc
AsliBasaran <112575572+AsliBasaran@users.noreply.github.com>
2022-12-25 00:40:26 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import './App.css';
+import { Users } from "./users"
+import Table from "./Table.js"
+
+function App() {
+  const [query, setQuery] = useState("");
+  const search = (data) => {
+    return data.filter(item => item.first_name.toLowerCase().includes(query) || item.last_name.toLowerCase().includes(query) || item.email.toLowerCase().includes(query));
+  }
+
+  return (
+    <div className="app">
+      <input type="text"
+        placeholder="Search"
+        className='search'
+        onChange={(e) => setQuery(e.target.value)} />
+
+      <Table data={search(Users)} />
+
+    </div>
+  );
+}
+
+export default App;
